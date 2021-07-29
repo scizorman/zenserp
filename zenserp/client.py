0d2fbfb5af51dbf8cdd81bd029a9ae157ab7bb3b
scizorman <tueda1207@gmail.com>
2021-07-30 04:35:31 +0900

    [change] Synchronous -> Asynchronous

--- zenserp/client.py
+++ zenserp/client.py
@@ -1,67 +1,57 @@
 from __future__ import annotations
 
 from types import TracebackType
-from typing import Mapping, Optional, Type, cast
+from typing import Iterable, Optional, Type, cast
 
-from requests import Response, Session
+from aiohttp import ClientSession
 
-from .exceptions import status_handler
-from .search import GL, HL, SERP, TBM, Device, Locations, SearchEngines, SearchInput
-from .status import Status
-
-STATUS_URL = "https://app.zenserp.com/api/v2/status"
-SEARCH_URL = "https://app.zenserp.com/api/v2/search"
-HL_URL = "https://app.zenserp.com/api/v2/hl"
-GL_URL = "https://app.zenserp.com/api/v2/hl"
-LOCATIONS_URL = "https://app.zenserp.com/api/v2/locations"
-SEARCH_ENGINES_URL = "https://app.zenserp.com/api/v2/search_engines"
+from .model import GL, HL, SERP, TBM, Device, Location, SearchEngine, Status
 
 
 class Client:
+
+    base_url = "https://app.zenserp.com/api/v2"
+
     def __init__(self, api_key: str) -> None:
-        """The client to request Zenserp.
+        """The asynchronous client to request Zenserp.
 
         Args:
             api_key (str): Your API key of Zenserp.
         """
-        self._session = Session()
-        self._session.headers["apikey"] = api_key
+        headers = {"apikey": api_key}
+        self.__session = ClientSession(headers=headers)
 
-    def __enter__(self) -> Client:
+    async def __aenter__(self) -> Client:
         return self
 
-    def __exit__(
+    async def __aexit__(
         self,
         exc_type: Optional[Type[BaseException]],
         exc_val: Optional[BaseException],
         exc_tb: Optional[TracebackType],
     ) -> None:
-        return self.close()
+        await self.close()
 
-    def close(self) -> None:
+    async def close(self) -> None:
         """Closes this client."""
-        return self._session.close()
+        await self.__session.close()
 
-    @status_handler
-    def _get(self, url: str, params: Optional[Mapping[str, str]] = None) -> Response:
-        with self._session.get(url, params=params) as resp:
-            resp.encoding = resp.apparent_encoding
-            return resp
-
-    def status(self) -> Status:
+    async def status(self) -> Status:
         """Checks the status of your API key.
 
         Returns:
             The status of your API key.
         """
-        with self._get(STATUS_URL) as resp:
-            return Status(resp.json()["remaining_requests"])
+        url = f"{self.base_url}/status"
+        async with self.__session.get(url) as resp:
+            body = await resp.json()
+            return Status(body["remaining_requests"])
 
-    def search(
+    async def search(
         self,
         query: str,
-        location: Optional[str] = None,
-        search_engine: Optional[str] = None,
+        location: Optional[Location] = None,
+        search_engine: Optional[SearchEngine] = None,
         limit: Optional[int] = None,
         offset: Optional[int] = None,
         tbm: Optional[TBM] = None,
@@ -99,56 +89,70 @@ class Client:
         Returns:
             Search results from the search via Zenserp.
         """
-        search_input = SearchInput(
-            query,
-            location=location,
-            search_engine=search_engine,
-            limit=limit,
-            offset=offset,
-            tbm=tbm,
-            device=device,
-            timeframe=timeframe,
-            gl=gl,
-            lr=lr,
-            hl=hl,
-            latitude=latitude,
-            longitude=longitude,
-        )
-        with self._get(SEARCH_URL, params=search_input.to_params()) as resp:
-            return cast(SERP, resp.json())
+        url = f"{self.base_url}/search"
+        params = {
+            k: v
+            for k, v in {
+                "q": query,
+                "location": location,
+                "search_engine": search_engine,
+                "num": limit,
+                "start": offset,
+                "tbm": None if tbm is None else tbm.value,
+                "device": None if device is None else device.value,
+                "timeframe": timeframe,
+                "gl": gl,
+                "lr": lr,
+                "hl": hl,
+                "lat": latitude,
+                "lng": longitude,
+            }.items()
+            if v is not None
+        }
+        async with self.__session.get(url, params=params) as resp:
+            body = await resp.json()
+            return cast(SERP, body)
 
-    def hl(self) -> HL:
+    async def hl(self) -> Iterable[HL]:
         """List all supported hl parameters.
 
         Returns:
             All supported hl parameters.
         """
-        with self._get(HL_URL) as resp:
-            return cast(HL, resp.json())
+        url = f"{self.base_url}/hl"
+        async with self.__session.get(url) as resp:
+            body = await resp.json()
+            return [HL(row["code"], row["name"]) for row in body]
 
-    def gl(self) -> GL:
+    async def gl(self) -> Iterable[GL]:
         """List all supported gl parameters.
 
         Returns:
             All supported gl parameters.
         """
-        with self._get(GL_URL) as resp:
-            return cast(GL, resp.json())
+        url = f"{self.base_url}/gl"
+        async with self.__session.get(url) as resp:
+            body = await resp.json()
+            return [GL(row["code"], row["name"]) for row in body]
 
-    def locations(self) -> Locations:
+    async def locations(self) -> Iterable[Location]:
         """List all supported geo locations.
 
         Returns:
             All supported geo locations.
         """
-        with self._get(LOCATIONS_URL) as resp:
-            return cast(Locations, resp.json())
+        url = f"{self.base_url}/locations"
+        async with self.__session.get(url) as resp:
+            body = await resp.json()
+            return [cast(Location, row) for row in body]
 
-    def search_engines(self) -> SearchEngines:
+    async def search_engines(self) -> Iterable[SearchEngine]:
         """List all supported Google search engines.
 
         Returns:
             All supported Google search engines.
         """
-        with self._get(SEARCH_ENGINES_URL) as resp:
-            return cast(SearchEngines, resp.json())
+        url = f"{self.base_url}/search_engines"
+        async with self.__session.get(url) as resp:
+            body = await resp.json()
+            return [cast(SearchEngine, row) for row in body]
--- zenserp/exceptions.py
+++ zenserp/exceptions.py
@@ -1,8 +1,4 @@
-from functools import wraps
-from typing import Any, Callable, Mapping
-from uuid import UUID
-
-from requests import Response
+from typing import Mapping
 
 
 class ZenserpException(Exception):
@@ -44,34 +40,3 @@ class InvalidRequestException(ZenserpException):
     def __str__(self) -> str:
         error_messages = ", ".join(f"{k}: {v.replace('.', '')}" for k, v in self._errors.items())
         return f"invalid request: ({error_messages})"
-
-
-def status_handler(func: Callable[..., Response]) -> Callable[..., Response]:
-    @wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Response:
-        resp = func(*args, **kwargs)
-        status_code = resp.status_code
-        if status_code == 200:
-            pass
-        elif status_code == 403:
-            message = resp.json()["error"]
-            if message == "No apikey provided.":
-                raise NoAPIKeyException("no API key is provided")
-            elif message == "Not enough requests.":
-                api_key = resp.request.headers["apikey"]
-                try:
-                    UUID(hex=api_key)
-                except ValueError:
-                    raise WrongAPIKeyException("your API key is wrong")
-                else:
-                    raise APILimitException("no request remains")
-        elif status_code == 404:
-            raise NotFoundException("not found")
-        elif status_code == 500:
-            errors = resp.json()["errors"][0]
-            raise InvalidRequestException(errors)
-        else:
-            resp.raise_for_status()
-        return resp
-
-    return wrapper
